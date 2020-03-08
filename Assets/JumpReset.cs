f2f42ec32e0c641c732492f751d5562b060ffea0
Zach <spywoawe@gmail.com>
2020-03-09 11:42:55 +1300

    Initial

---
+++ Assets/JumpReset.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpReset : MonoBehaviour
+{
+    public PlayerMovement player;
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        player.ResetJump();
+    }
+}
