97404d365a73810aba87725a039eeeebd07fee59
Aidan Robinson <aidanrobinson6@gmail.com>
2020-03-19 09:21:26 +1300

    Scipting TitleScreen V1

---
+++ Assets/Scripts/UI_Scripts/Title_Screen/AudioMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetMasterVolume(float volume)
+    {
+        Debug.Log(volume);
+    }
+
+    public void SetEffectsVolume(float volume)
+    {
+        Debug.Log(volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        Debug.Log(volume);
+    }
+}
