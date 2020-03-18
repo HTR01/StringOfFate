1507e3ec60887f43a3109d93a796f9be660b2a93
Zach <spywoawe@gmail.com>
2020-03-19 11:37:17 +1300

    Barrel, Crate, Adding Transport Log

---
+++ Assets/Scripts/pickupTransport.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupTransport : MonoBehaviour
+{
+    //If object enters and layer is == to pickups, destroy object and instantiate it at X paired position.
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
