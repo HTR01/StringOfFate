ab7967eeec8cfa5bcbae80f2e3d75d384800bf51
Zach <spywoawe@gmail.com>
2020-03-20 08:57:12 +1300

    Transport Log Working

--- Assets/Scripts/pickupTransport.cs
+++ Assets/Scripts/pickupTransport.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class pickupTransport : MonoBehaviour
 {
     //If object enters and layer is == to pickups, destroy object and instantiate it at X paired position.
-
+    public GameObject pickupFab;
+    public Transform telePos;
 
     void Start()
     {
@@ -16,5 +17,14 @@ public class pickupTransport : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            Destroy(other.gameObject);
+            Instantiate(pickupFab, telePos.position, telePos.rotation);
+        }
     }
 }
