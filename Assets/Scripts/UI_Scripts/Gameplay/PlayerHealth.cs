e82364943bdc30f94ade6bb67663e993b9da6c66
Aidan Robinson <aidanrobinson6@gmail.com>
2020-04-09 13:20:04 +1200

    Health  Bar works by pressing Q and Z right now

---
+++ Assets/Scripts/UI_Scripts/Gameplay/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    private Stat health;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        health.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            health.CurrentVal -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            health.CurrentVal += 10;
+        }
+    }
+}
