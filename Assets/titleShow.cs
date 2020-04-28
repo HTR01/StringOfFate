0d7450dcb55e938df1774b8f67a0c3fc567f165f
Zach <spywoawe@gmail.com>
2020-04-29 10:11:40 +1200

    Title in credits (play not working yet, just having this here)

---
+++ Assets/titleShow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class titleShow : MonoBehaviour
+{
+    public GameObject credits;
+    public GameObject title;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            credits.SetActive(false);
+            title.SetActive(true);
+        }
+    }
+}
