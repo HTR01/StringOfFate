ad18545e927324482c916c248973292b835fe416
Zach <spywoawe@gmail.com>
2020-04-30 15:10:23 +1200

    Music

---
+++ Assets/Scripts/cameraTrack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraTrack : MonoBehaviour
+{
+    public Vector3 camPosRight;
+    public Vector3 camPosLeft;
+
+    public KeyCode left, right;
+    public PlayerMovement playM;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playM.horiz == 1)
+        {
+            Vector3 temp2 = camPosRight;
+            this.transform.position = temp2;
+        }
+
+        if (playM.horiz == -1)
+        {
+            Vector3 temp2 = camPosRight;
+            this.transform.position = temp2;
+        }
+    }
+}
